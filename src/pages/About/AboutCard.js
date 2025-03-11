f34931cc61687485e733ce235097e7cb7da77fba
Rubayat Al Imran <rubayat@vertis.digital>
2025-03-11 21:00:02 +0600

    update content

--- src/pages/About/AboutCard.js
+++ src/pages/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
             Hi Everyone, I am <span className="purple">Rubayat Imran </span>
             from <span className="purple"> Dhaka, Bangladesh.</span>
             <br />
-            I'm a Software Engineer who loves turning ideas into reality with code. I’ve got a good bit of experience creating custom websites using ReactJs, Redux, JavaScript, TypeScript, jQuery, HTML5, and CSS3.
+            I am an experienced front-end developer and an Optimizely-certified CRO (A/B testing) expert with a passion for writing high-quality code and delivering bug-free tests. I bring over 5 years of experience in building high-quality, performant websites and applications using modern tools and frameworks such as React, Redux, JavaScript, TypeScript, jQuery, Sass/Less, Bootstrap, and other frontend libraries.
             <br />
             I enjoy learning new things and keeping up with the latest in technology. My goal is always to deliver high-quality and efficient solutions that make a difference.
             <br />
@@ -28,6 +28,9 @@ function AboutCard() {
             </li>
             <li className="about-activity">
               <ImPointRight /> Listening to music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Trying new foods
             </li>
           </ul>
           <p style={{ textAlign: "justify" }}>These activities help me stay balanced and bring a fresh perspective to my work.</p>
